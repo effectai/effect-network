bdcfcff43a95dbad2b7565364ef358d4e0786d32
Jesse Eisses <jesse.eisses@gmail.com>
2019-04-01 21:52:39 +0200

    Keep track of issued swap transactions

--- contracts/swap/swap.cpp
+++ contracts/swap/swap.cpp
@@ -9,7 +9,8 @@ using namespace eosio;
 
 class [[eosio::contract]] swap : public contract {
 public:
-  swap(eosio::name receiver, eosio::name code, eosio::datastream<const char*> ds) : eosio::contract(receiver, code, ds), _nep5(_self, _self.value) {};
+  swap(eosio::name receiver, eosio::name code, eosio::datastream<const char*> ds) :
+    eosio::contract(receiver, code, ds), _nep5(_self, _self.value), _issued(_self, _self.value) {};
 
   capi_checksum256 neo_hash(std::vector<char> data) {
     capi_checksum256 digest, blockhash;
@@ -27,7 +28,8 @@ public:
                                 const fixed_bytes<20> asset_hash, const int64_t value) {
     require_auth(permission_level{get_self(), "active"_n});
     auto id =  _nep5.available_primary_key();
-    _nep5.emplace(_self, [&](auto& n) {
+    _nep5.emplace(_self, [&](auto& n)
+                         {
                            n.id = id;
                            n.txid = txid;
                            n.asset_hash = asset_hash;
@@ -43,6 +45,14 @@ public:
     auto txids = _nep5.get_index<"txid"_n>();
     checksum256 chk = (checksum256) txhash.hash;
     auto& tx = txids.get(chk, "tx not found");
+
+    auto issued = _issued.find(tx.id);
+    eosio::check(issued == _issued.end(), "tx already issued");
+
+    _issued.emplace(_self, [&](auto& n)
+                           {
+                             n.id = tx.id;
+                           });
 
     // TODO: fetch precision from token stat table
     symbol sym = symbol(token, 4);
@@ -67,6 +77,14 @@ private:
 
   typedef multi_index<"nep5"_n, nep5, indexed_by<"txid"_n, const_mem_fun<nep5, checksum256, &nep5::by_txid>>> nep5_table;
   nep5_table _nep5;
+
+  struct [[eosio::table]] issued {
+    uint64_t id;
+    uint64_t primary_key() const { return id; }
+  };
+
+  typedef multi_index<"issued"_n, issued> issued_table;
+  issued_table _issued;
 };
 
 EOSIO_DISPATCH(swap, (checktx)(posttx)(issue));
