566dae2ddaf4157554860c7a50e2f098ba9004c7
Jesse Eisses <jesse.eisses@gmail.com>
2019-04-10 14:16:49 +0200

    Remove unused issued table

--- contracts/swap/swap.hpp
+++ contracts/swap/swap.hpp
@@ -14,7 +14,7 @@ using namespace eosio;
 class [[eosio::contract("swap")]] swap : public contract {
  public:
   swap(eosio::name receiver, eosio::name code, eosio::datastream<const char*> ds) :
-    eosio::contract(receiver, code, ds), _nep5(_self, _self.value), _issued(_self, _self.value),
+    eosio::contract(receiver, code, ds), _nep5(_self, _self.value),
     _bookkeeper(_self, _self.value) {};
 
   [[eosio::action]]
@@ -50,21 +50,14 @@ class [[eosio::contract("swap")]] swap : public contract {
     checksum256 by_txid() const { return txid; }
   };
 
-  struct [[eosio::table]] issued {
-    uint64_t id;
-    uint64_t primary_key() const { return id; }
-  };
-
   struct [[eosio::table]] bookkeeper {
     name account;
     uint64_t primary_key() const { return account.value; }
   };
 
   typedef multi_index<"nep5"_n, nep5, indexed_by<"txid"_n, const_mem_fun<nep5, checksum256, &nep5::by_txid>>> nep5_table;
-  typedef multi_index<"issued"_n, issued> issued_table;
   typedef multi_index<"bookkeeper"_n, bookkeeper> bookkeeper_table;
 
   nep5_table _nep5;
-  issued_table _issued;
   bookkeeper_table _bookkeeper;
 };
