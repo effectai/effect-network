3e58a6603e34b7c260a921e44361de355b04a2ad
Jesse Eisses <jesse.eisses@gmail.com>
2019-04-16 14:42:14 +0200

    Update local deployment script and readme

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ EOS smart contracts for The Effect Network
 
 ## Compiling
 
-The token contract is implemented in C++. To compile and deploy the
-contracts you will need the following prerequisites:
+The token contract is implemented in C++. To compile the contracts you
+will need the following prerequisites:
 
 - eosio.cdt v1.5.0
 - eosio v1.6.4
@@ -23,4 +23,28 @@ You can also use `eosio-cpp` to compile individual contracts:
 ```bash
 cd contracts/effect-token
 eosio-cpp -o effect-token.wasm effect-token.cpp --abigen
+```
+
+## Deploying
+
+To deploy the contracts to a [local EOSIO
+testnet](https://github.com/EOSIO/eos/tree/master/Docker) you can use
+the provided setup script:
+
+```bash
+npm install
+npm run deploy local efxefxefxefx swapswapswap stakesstakes
+```
+
+This script uses the default `eosio` account to deploy, initialize and
+authorize the accounts.
+
+Some actions that can be used to issue and stake tokens:
+
+```bash
+cleos push action swapswapswap posttx '["eosio","d10153081027000000000000141b00234a5dcafb17ae645c203617f709450e8c5b149c49f34d4ef20298122900697b68c7526f7bf91e53c1087472616e7366657267f9e6e770af783d809bd1a65e1bb5b6042953bcac000000000000000003209c49f34d4ef20298122900697b68c7526f7bf91ef0056a65737365f00d313535343330353937393937390000","eosio","acbc532904b6b51b5ea6d19b803d78af70e7e6f9","10000000000"]' -p eosio@active
+cleos push action swapswapswap issue '["6a8f4f8c5bc3f44deff460ef53b787335096e7df67d3d229fb41779cf2b919bf"]' -p eosio@active
+cleos push action efxefxefxefx transfer '["eosio","stakesstakes","350000.0000 EFX","stake"]' -p eosio@active
+cleos push action stakesstake claim '["eosio","EFX"]' -p eosio@active
+cleos get table efxefxefxefx eosio accounts
 ```
--- deploy/local.cljs
+++ deploy/local.cljs
@@ -6,27 +6,59 @@
    (clojure.pprint :refer [pprint])))
 
 (defn usage [opts]
-  (->> ["Run as `npm run deploy <owner> <token contract> <swap contract>"
-        ""]
+  (->> ["Run as `npm run deploy <token contract> <swap contract> <stake contract>"
+        ""
+        "The `eosio` account will be used to setup other accounts
+        using the default keys in a privnet."]
        (string/join "\n")))
 
-(defn setup [owner-acc token-acc swap-acc asset]
-  (print "\n========================\nCREATE TOKEN" asset "\n========================\n")
-  (->
-   (eos/transact token-acc "create" {:issuer swap-acc :maximum_supply asset})
-   (.then #(do (print "> Token created") (eos/wait-block %)))
-   ;; (.then (eos/transact token-acc "issue" {:to owner-acc :quantity "1 EFX" :memo "issue"}))
-   ;; (.then #(do (print "\n> 1 token issued to" owner-acc) (eos/wait-block %)))
-   (.catch prn)))
+(def efx-supply "650000000.0000 EFX")
+(def nfx-supply "20000000000.0000 NFX")
+(def efx-sym "EFX")
+(def nfx-sym "NFX")
+
+(def stake-config {:token_contract token-acc :stake_symbol efx-sym
+                   :claim_symbol nfx-sym :age_limit 5 :scale_factor (*  1000000 1)
+                   :unstake_delay_sec 2})
+
+(def swap-config {:token_contract token-acc :token_symbol efx-sym
+                  :issue_memo "Token Swap"})
+
+(defn setup [owner-acc token-acc swap-acc stake-acc]
+  (print "\n========================\nCREATE TOKENS" "\n========================\n")
+  (-> (eos/transact token-acc "create" {:issuer swap-acc :maximum_supply efx-supply})
+      (.catch prn)      
+      (.then #(println "> Token created " efx-sym))
+      (.then #(eos/transact token-acc "create"
+                            {:issuer stake-acc :maximum_supply nfx-supply}))
+      (.catch prn)      
+      (.then #(println "> Token created " nfx-sym))        
+      (.then #(eos/transact stake-acc "init"
+                            (assoc stake-config :token_contract token-acc)
+                            [{:actor stake-acc :permission "owner"}]))
+      (.catch prn)
+      (.then #(eos/transact swap-acc "init"
+                            (assoc swap-config :token_contract token-acc)))
+      (.catch prn)
+      (.then #(eos/transact swap-acc "mkbookkeeper" {:account "eosio"}))
+      (.catch prn)))
 
 (defn -main []
-  (let [[owner-acc token-acc swap-acc asset] *command-line-args*]
+  (let [[token-acc swap-acc stake-acc] *command-line-args*
+        owner-acc "eosio"]
     (->
      (eos/create-account owner-acc token-acc)
      (.catch prn)
      (.then #(eos/create-account owner-acc swap-acc))
      (.catch prn)
-     (.then eos/wait-block)
+     (.then #(eos/create-account owner-acc stake-acc))     
+     (.catch prn)
+     (.then #(eos/deploy token-acc "contracts/effect-token/effect-token"))
+     (.catch prn)
+     (.then #(eos/deploy swap-acc "contracts/swap/swap"))
+     (.catch prn)
+     (.then #(eos/deploy stake-acc "contracts/stake/stake"))
+     (.catch prn)
      (.then #(eos/update-auth swap-acc "active"
                               [{:permission
                                 {:actor owner-acc :permission "active"}
@@ -34,13 +66,10 @@
                                {:permission
                                 {:actor swap-acc :permission "eosio.code"}
                                 :weight 1}]))
-     (.then #(eos/update-auth token-acc "active" swap-acc "active"))
-     (.then eos/wait-block)
-     (.then #(eos/deploy token-acc "contracts/effect-token/src/effect-token"))
-     (.catch prn)
-     (.then #(eos/deploy swap-acc "contracts/swap/swap"))
-     (.catch prn)
-     (.then eos/wait-block)
-     (.then #(setup owner-acc token-acc swap-acc "650000000.0000 EFX"))
-
+     (.then #(eos/update-auth stake-acc "active"
+                              [{:permission {:actor token-acc :permission "eosio.code"}
+                                :weight 1}
+                               {:permission {:actor stake-acc :permission "eosio.code"}
+                                :weight 1}]))
+     (.then #(setup owner-acc token-acc swap-acc stake-acc))
      (.then #(print "\nDone!\n")))))
