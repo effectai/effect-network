06d7ab810d577a090fe0323030d4359e783e1e02
Jesse Eisses <jesse.eisses@gmail.com>
2021-01-28 14:45:28 +0100

    Create shared header for proposal structs

--- contracts/effect-proposals/effect-proposals.hpp
+++ contracts/effect-proposals/effect-proposals.hpp
@@ -8,8 +8,10 @@
 #include <eosio/singleton.hpp>
 #include <eosio/binary_extension.hpp>
 
+#include "effect-proposals-shared.hpp"
+
 using namespace eosio;
-using namespace std;
+using namespace proposal;
 
 class [[eosio::contract("effect-proposals")]] proposals : public eosio::contract {
 public:
@@ -95,15 +97,6 @@ public:
                     uint64_t id);
 
 private:
-  struct [[eosio::table]] config {
-    uint32_t cycle_duration_sec;
-    uint32_t cycle_voting_duration_sec;
-    eosio::extended_asset proposal_cost;
-    uint32_t quorum;
-    uint16_t current_cycle;
-    eosio::name dao_contract;
-  };
-
   struct [[eosio::table]] cycle {
     uint64_t id;
     eosio::time_point_sec start_time;
@@ -147,24 +140,6 @@ private:
   //   uint64_t primary_key() const { return cycle; }
   // };
 
-  struct [[eosio::table]] vote {
-    uint64_t id;
-    eosio::name voter;
-    uint64_t proposal_id;
-    uint8_t type;
-    uint32_t weight;
-    std::optional<eosio::name> delegatee;
-    std::optional<std::string> comment_hash;
-
-    uint64_t primary_key() const { return id; }
-    uint128_t composite_key() const { return (uint128_t{proposal_id} << 64) | voter.value; }
-    uint64_t by_voter() const { return voter.value; }
-    uint64_t by_proposal() const { return proposal_id; }
-
-    EOSLIB_SERIALIZE(vote, (id)(voter)(proposal_id)(type)(weight)(delegatee)
-                     (comment_hash));
-  };
-
   typedef multi_index<
     "proposal"_n, proposal,
     indexed_by<"author"_n, const_mem_fun<proposal, uint64_t, &proposal::by_author>>,
@@ -172,14 +147,6 @@ private:
   proposal_table;
   typedef multi_index<"cycle"_n, cycle> cycle_table;
   typedef multi_index<"reservation"_n, reservation> reservation_table;
-  typedef multi_index<
-    "vote"_n, vote,
-    indexed_by<"composite"_n, const_mem_fun<vote, uint128_t, &vote::composite_key>>,
-    indexed_by<"voter"_n, const_mem_fun<vote, uint64_t, &vote::by_voter>>,
-    indexed_by<"proposal"_n, const_mem_fun<vote, uint64_t, &vote::by_proposal>>>
-  vote_table;
-
-  typedef singleton<"config"_n, config> config_table;
 
   config_table _config;
 };
