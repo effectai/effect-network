743722be42b32c5c3d3ab8949233541fc04c23bb
Laurens Verspeek <l.verspeek@gmail.com>
2021-04-26 18:06:47 +0200

    read block header correctly + start on getting bookkeepers from pubkey list

--- contracts/ccmc/include/ccmc.hpp
+++ contracts/ccmc/include/ccmc.hpp
@@ -2,7 +2,6 @@
 #include <eosio/singleton.hpp>
 #include <eosio/crypto.hpp>
 
-
 using namespace std;
 using namespace eosio;
 
@@ -10,7 +9,7 @@ CONTRACT ccmc : public contract {
   public:
     using contract::contract;
 
-    ACTION initgenblock(vector<char> raw_header);
+    ACTION initgenblock(vector<char> raw_header, vector<char> pubkey_list);
 
     static const uint32_t PUBKEY_LEN = 67;
     static const uint32_t SIG_LEN = 65;
@@ -18,23 +17,54 @@ CONTRACT ccmc : public contract {
     struct header {
       uint32_t version;
       uint64_t chainId;
-      vector<char> prevBlockHash;
-      vector<char> transactionsRoot;
-      vector<char> crossStatesRoot;
-      vector<char> blockRoot;
+      checksum256 prevBlockHash;
+      checksum256 transactionsRoot;
+      checksum256 crossStatesRoot;
+      checksum256 blockRoot;
       uint32_t timestamp;
       uint32_t height;
       uint64_t consensusData;
-      vector<char> consensusPayload;
-      vector<char> nextBookkeeper;
+      checksum160 nextBookkeeper;
     };
 
-   header deserialize_header(vector<char> raw_header) {
-      // checksum256 header_digest = sha256(raw_header.data(), raw_header.size());
-      // print("header_digest ", header_digest, "\n");
-      cmcc:header header = unpack<ccmc::header>(raw_header);;
+    struct bookkeeper {
+      vector<checksum160> keepers;
+      checksum160 nextBookkeeper;
+    };
+
+
+    header deserialize_header(vector<char> raw_header) {
+      cmcc:header header = unpack<ccmc::header>(raw_header);  
+      // We are skipping the variable part consensusPayload as we don't need it, instead we just read the last 20 bytes
+      // into nextBookkeeper. TODO: figure out a way of getting last 20 bytes without unpack
+      vector<char> temp = vector<char>(raw_header.end() - 20, raw_header.end());
+      
+      header.nextBookkeeper = unpack<checksum160>(temp);
       return header;
     }
+    bookkeeper verify_pubkey(vector<char> pubkey_list) {
+      eosio::check(pubkey_list.size() % PUBKEY_LEN == 0, "length of pubkey_list is invalid");
+      uint32_t n = pubkey_list.size() / PUBKEY_LEN;
+      uint32_t m = n - (n - 1) / 3;
+      cmcc:bookkeeper bookkeeper = ccmc::bookkeeper();
+      return get_bookkeeper(n,  n - (n - 1) / 3, pubkey_list);
+    }
+    bookkeeper get_bookkeeper(uint32_t key_len, uint32_t m, vector<char> pubkey_list) {
+      vector<checksum160> keepers(key_len);
+      // for(uint i = 0; i < _keyLen; i++){
+      //     publicKey = Utils.slice(_pubKeyList, i*POLYCHAIN_PUBKEY_LEN, POLYCHAIN_PUBKEY_LEN);
+      //     buff =  abi.encodePacked(buff, ZeroCopySink.WriteVarBytes(Utils.compressMCPubKey(publicKey)));
+      //     hash = keccak256(Utils.slice(publicKey, 3, 64));
+      //     keepers[i] = address(uint160(uint256(hash)));
+      // }
+      // buff = abi.encodePacked(buff, ZeroCopySink.WriteUint16(uint16(_m)));
+      // bytes20  nextBookKeeper = ripemd160(abi.encodePacked(sha256(buff)));
+      cmcc:bookkeeper bookkeeper = ccmc::bookkeeper();
+      // bookkeeper.keepers = keepers
+      // bookkeeper.nextBookkeeper = nextBookkeeper
+      return bookkeeper;
+    }
+    
 
   private:
     TABLE poly {
--- contracts/ccmc/src/ccmc.cpp
+++ contracts/ccmc/src/ccmc.cpp
@@ -1,15 +1,25 @@
 #include <ccmc.hpp>
 
-ACTION ccmc::initgenblock(std::vector<char> raw_header) {
+ACTION ccmc::initgenblock(std::vector<char> raw_header, std::vector<char> pubkey_list) {
   // require_auth(get_self());
   poly_table poly_tbl(_self, _self.value);
-  eosio::check(!poly_tbl.exists(), "already initialized");
-  //eosio::check(pubkey_list.size() % PUBKEY_LEN == 0, "length of pubkey_list is invalid");
+  //eosio::check(!poly_tbl.exists(), "already initialized");
+  print("size: ", pubkey_list.size());
   
+  ccmc:bookkeeper bookkeeper = verify_pubkey(pubkey_list);
   cmcc:header header = deserialize_header(raw_header);
-  eosio::print("chain_id:", header.chainId);
-  eosio::print("height: ", header.height);
-  poly_tbl.set(poly{header.height}, get_self());
+  eosio::print("version:", header.version, "\n");
+  eosio::print("chain_id:", header.chainId, "\n");
+  eosio::print("height: ", header.height, "\n");
+  eosio::print("timestamp: ", header.timestamp, "\n");
+  eosio::print("consensusData: ", header.consensusData, "\n");
+  eosio::print("header.transactionsRoot: ", header.transactionsRoot, "\n");
+  eosio::print("header.crossStatesRoot: ", header.crossStatesRoot, "\n");
+  eosio::print("header.blockRoot: ", header.blockRoot, "\n");
+  eosio::print("header.nextBookkeeper: ", header.nextBookkeeper, "\n");
+  
+  //eosio::check(header.nextBookkeeper == bookkeeper.nextBookkeeper, "nextBookkeeper illegal");
+  //poly_tbl.set(poly{header.height, serializeKeepers(bookkeeper.keepers)}, get_self());
 }
 
 EOSIO_DISPATCH(ccmc, (initgenblock))
