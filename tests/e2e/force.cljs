52a8c63c700a0066a46d7065825829afef36f486
Jesse Eisses <jesse.eisses@gmail.com>
2022-10-27 14:44:45 +0200

    Remove proxy test

--- tests/e2e/force.cljs
+++ tests/e2e/force.cljs
@@ -26,7 +26,7 @@
 (def vacc-acc (eos/random-account "vacc"))
 (def token-acc (eos/random-account "tkn"))
 (def force-acc (eos/random-account "force"))
-(def proxy-acc (eos/random-account "proxy"))
+;; (def proxy-acc (eos/random-account "proxy"))
 (def fee-acc (eos/random-account "fee"))
 
 (println "force-acc" force-acc)
@@ -72,10 +72,9 @@
           (<p-may-fail! (eos/create-account owner-acc force-acc))
           (<p-may-fail! (eos/create-account owner-acc fee-acc))
           (<p! (eos/create-account owner-acc vacc-acc))
-          (<p! (eos/create-account owner-acc proxy-acc))
           (<p! (deploy-file vacc-acc "contracts/vaccount/vaccount"))
           (<p-may-fail! (deploy-file force-acc "contracts/force/force"))
-          (<p! (deploy-file proxy-acc "contracts/taskproxy/taskproxy"))
+          ;; (<p! (deploy-file proxy-acc "contracts/taskproxy/taskproxy"))
           (<!  (e2e.token/deploy-token token-acc [owner-acc token-acc]))
 
           (<p! (eos/update-auth vacc-acc "xfer" "active"
@@ -855,16 +854,16 @@
                                  {:payment_id 1
                                   :sig (sign-params params-1)})))))
 
-(async-deftest proxy
-  (testing "proxy can create campaign"
-    (<p-should-succeed!
-     (tx-as proxy-acc force-acc "mkcampaign"
-            {:owner ["name" proxy-acc]
-             :content {:field_0 0 :field_1 vacc/hash160-1}
-             :reward {:quantity "0.0000 EFX" :contract token-acc}
-             :qualis []
-             :payer proxy-acc
-             :sig nil}))))
+;; (async-deftest proxy
+;;   (testing "proxy can create campaign"
+;;     (<p-should-succeed!
+;;      (tx-as proxy-acc force-acc "mkcampaign"
+;;             {:owner ["name" proxy-acc]
+;;              :content {:field_0 0 :field_1 vacc/hash160-1}
+;;              :reward {:quantity "0.0000 EFX" :contract token-acc}
+;;              :qualis []
+;;              :payer proxy-acc
+;;              :sig nil}))))
 
 (defn -main [& args]
   (run-tests))
