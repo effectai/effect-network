3729a35e46c65b32de8f285d30698fed8465bcd8
Jesse Eisses <jesse.eisses@gmail.com>
2023-10-11 13:47:04 +0200

    ci: Update guix manifest to support docker builds

--- manifest.scm
+++ manifest.scm
@@ -1,6 +1,6 @@
-(use-modules (blockchain)
-             (gnu packages gcc)
-             (gnu packages node))
+(use-modules
+ (gnu packages gcc)
+ (gnu packages node))
 
 (packages->manifest
- (list (list gcc "lib") node eosio-cdt gnu-make))
+ (list (list gcc "lib") node  gnu-make))
